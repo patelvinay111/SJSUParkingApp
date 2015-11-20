0e72e7cd0c864a1a933c8c3fc2d81f7d9f8da16d
Eric Pham <epham5413@gmail.com>
2015-11-19 23:34:33 -0800

    Updated status and provider with scripts

--- provider.php
+++ provider.php
@@ -1,65 +1,37 @@
-<!DOCTYPE html>
-<html lang "en-US">
-<head>
-        <title>SJSU Parking app</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="js/jquery-ui-1.11.4.custom/jquery-ui.css">
 
-        <style>
-        	.header{
-        		float: left;
-        	}
-        	.logo{
-        		padding-top: 10px;
-        	}
-        	.spinners{
-        		padding-left: 20%;
-        	}
-        </style>
-    
-    </head>
+   <?php        
+    $floorNum = filter_input(INPUT_GET, "floor");
+    $availableSpaces = filter_input(INPUT_GET, "spaces");
+    $garageName = filter_input(INPUT_GET, "garage");
+    //$floorNum = $_POST['floor'];
+    //$availableSpaces = $_POST['spaces'];
+    //$garageName = $_POST['garage'];
 
-    <body>
-        <a class="btn btn-primary" 
-           type="button" 
-           href="index.html" 
-           style="font-size: 20px; width: 100px; margin: 20px 20px;">Back</a>
-         
-        <?php        
-        $floorNum = filter_input(INPUT_GET, "floor");
-        $availableSpaces = filter_input(INPUT_GET, "spaces");
-        $garageName = filter_input(INPUT_GET, "garage");
-            
-        try{    
+    try{    
         // Connect to the database.
         $con = new PDO("mysql:host=localhost;dbname=SJSU_Parking",
                            "root", "myfirstDB");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         $query = "INSERT INTO Report (userID, garage, floor, spaces) ".
                 "VALUES (1, '$garageName', :floorNum, :availableSpaces)";
-        
+
         $ps = $con->prepare($query);
         $ps->bindParam(':floorNum', $floorNum);
         $ps->bindParam(':availableSpaces', $availableSpaces);
         
         if($ps->execute() === TRUE){
-            $message = "Thank you!";
-            echo "  <script type='text/javascript'>
-                        alert('$message');
-                    </script>";
+            //$message = "Thank you!";            
+            //echo "<script>setTimeout(\"location.href = 'http://localhost/SJSUParkingApp/index.html';\",0);</script>";                  
+            //echo "<script  type='text/javascript'> alert('$message'); </script>";
+            //exit();
+            
+            header("Location:http://localhost/SJSUParkingApp/index.html");
+            exit();
         }else
             echo "Error: " . $sql . "<br>" . $con->error;
-            
-        }
-        catch(Exception $ex) {
-            print 'ERROR: '.$ex->getMessage();
-        }        
-        ?>
-        
-    </body>
-
-
-</html>
+    }
+    catch(Exception $ex) {
+        print 'ERROR: '.$ex->getMessage();
+    }        
+?>
